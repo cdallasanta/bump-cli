f7ede8762d436b769e8611f1a94da77ca030991f
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-03-09 04:16:46 +0000

    starting scraper

---
+++ lib/bump/scraper.rb
@@ -0,0 +1,11 @@
+class Scraper
+  attr_accessor :stage
+
+  def set_stage
+    puts "What stage of pregnancy are you in?"
+    puts "Please select the number from the following options:"
+  end
+
+  def get_articles
+  end
+end
