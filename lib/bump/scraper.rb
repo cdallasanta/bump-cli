ac10c27e851766779bfee4edbeb1fb822bb4282e
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-03-09 16:55:29 +0000

    working on scraper

--- lib/bump/scraper.rb
+++ lib/bump/scraper.rb
@@ -1,3 +1,6 @@
+require 'nokogiri'
+require 'open-uri'
+
 class Scraper
   attr_accessor :stage
 
@@ -17,20 +20,39 @@ class Scraper
     choice = gets.chomp
     case choice
     when 1
-      @stage = "Planning a Family"
+      @stage = "getting-pregnant"
     when 2
-      @stage = "First Trimester"
+      @stage = "first-trimester"
     when 3
-      @stage = "Second Trimester"
+      @stage = "second-trimester"
     when 4
-      @stage = "Third Trimester"
+      @stage = "third-trimester"
     when 5
-      @stage = "Parenting"
+      @stage = "parenting"
     else
       @stage = ""
     end
   end
 
   def get_articles
+    html = Nokogiri::HTML(open("https://www.thebump.com"))
+    all_sections = html.css(".homepage-panel---articles")
+    all_sections[@stage].css(".homepage-panel--item").each do |article|
+      puts article.attribute("href").value
+    end
+=begin
+    all_cards.each do |card|
+      article_url = card.css("a").attribute("href").value
+      scrape_article(article_url)
+    end
+=end
+  end
+
+  def scrape_article(url)
+    puts url
   end
 end
+
+scraper = Scraper.new
+scraper.stage = ""
+scraper.get_articles
